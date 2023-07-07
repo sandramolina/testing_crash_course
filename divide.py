4e08c49e9d124388e2eb082269177a640e6b0acf
Sandra Molina Palencia <smolinapalencia@gmail.com>
2023-07-07 17:05:07 +0100

    adds more tests and creates function to handle user input

--- divide.py
+++ divide.py
@@ -1,9 +1,25 @@
-# numerator = int(input("Please enter your numerator: "))
-# denominator = int(input("Please enter your denominator: "))
+class IncorrectInput(Exception):
+    pass
 
 
 def divide(numerator=1, denominator=2):
-    return (numerator/denominator)
+    try:
+        return (numerator/denominator)
+    except ZeroDivisionError as e:
+        print("⚠️ Error:", e)
+        raise ZeroDivisionError
+    except TypeError as e:
+        print("⚠️ Error:", e)
+        raise IncorrectInput
 
 
-# print(divide(1, 1))
+def get_user_input():
+    numerator = int(input("Please enter your numerator: "))
+    denominator = int(input("Please enter your denominator: "))
+    return numerator, denominator
+
+# HOW TO USE THE FUNCTIONS:
+# user_input = get_user_input()
+# num1 = user_input[0]
+# num2 = user_input[1]
+# print(divide(num1, num2))
--- test_divide.py
+++ test_divide.py
@@ -1,10 +1,21 @@
 import unittest
-from divide import divide
+from divide import divide, IncorrectInput
 
 
-class MainTestCase(unittest.TestCase):
+class DivideTestCase(unittest.TestCase):
+    def test_divide_valid_default(self):
+        self.assertEqual(0.5, divide())
+
     def test_divide_valid_8_4(self):
         self.assertEqual(2, divide(8, 4))
+
+    def test_divide_invalid_strings(self):
+        with self.assertRaises(IncorrectInput):
+            divide("3", "7")
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(3, 0)
 
 
 if __name__ == '__main__':
